6174ec34b2e569526dcf5341dd7d087466e105b3
Mohammad_Es0281 <mohammad.es0281@gmail.com>
2022-02-02 17:34:09 +0330

    Exercise-7 done

--- Navigation Exercises/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ Navigation Exercises/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -43,8 +43,11 @@ class GameWonFragment : Fragment() {
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
         binding.nextMatchButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+            view.findNavController().navigate(
+                GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
         return binding.root
     }
 }
