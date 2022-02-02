7b8be05422d427e03e1d1a208a24faacce80ca22
Mohammad_Es0281 <mohammad.es0281@gmail.com>
2022-02-02 19:44:59 +0330

    Exercise-10 done

--- Navigation Exercises/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ Navigation Exercises/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -16,10 +16,12 @@
 
 package com.example.android.navigation
 
+import androidx.databinding.DataBindingUtil
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -27,15 +29,20 @@ import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var appBarConfiguration : AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding =
-            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         drawerLayout = binding.drawerLayout
         val navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        navController.addOnDestinationChangedListener { itNavController: NavController, navDestination: NavDestination, _ ->
+            if (navDestination.id == itNavController.graph.startDestination)
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            else
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+        }
         NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
